7becb7be2c3920aeb1dfdbb7e8858eb016f2e686
Erik work 2 <braniubojni@mail.ru>
2021-08-06 17:27:24 +0400

    Homework06.08.21

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Input from './components/Input';
+import Button from './components/Button';
+import H1 from './components/H1';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    value: 0,
+    left: "",
+    right: "",
+  }
+  doIncrement = () => {
+    this.setState({value: this.state.value + 1})
+  }
+  doDecrement = () => {
+    this.setState({value: this.state.value - 1})
+  }
+  add = () => {
+    const left = this.Input
+    this.setState()
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <H1 title={this.state.value}></H1>
+        <div className="inputs">
+          <Input type="text" placeholder="Type any number" value={this.state.left} />
+          <Input type="text" placeholder="Type any number" value={this.state.right} />
+        </div>
+        
+        <div className="buttons">
+          <Input type="checkbox"/>
+          <Button 
+            className="myButton" 
+            onClick={this.doDecrement}>
+              Decrement
+          </Button>
+          <Button onClick={this.doIncrement} className="myButton">Increment</Button>
+        </div>
+        <Button onClick={this.add} className="myButton">+</Button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
